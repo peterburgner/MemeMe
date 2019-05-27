4660db582169c755588026a57b5bf1c8c1208dac
Peter Burgner <dev@burgner.de>
2019-05-27 16:29:15 -0700

    Add basic view controller classes

---
+++ MemeMe/MemeCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Peter Burgner on 5/27/19.
+//  Copyright © 2019 Peter Burgner. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+    
+}
