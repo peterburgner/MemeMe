9ad4ee56051a2d744bc5104649045d9c22d36e0b
Peter Burgner <dev@burgner.de>
2019-05-27 16:43:56 -0700

    Implement basic table view with hard coded defaults

--- MemeMe/MemeTableViewController.swift
+++ MemeMe/MemeTableViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class MemeTableViewController: UITableViewController {
     
+    // MARK: Properties
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let memes = [Meme(topText: "Top text", bottomText: "Bottom", originalImage: UIImage(), memedImage: UIImage())]
+    
+    
+    // MARK: Table Functions
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: Add detail view
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTableViewCell")!
+        
+        cell.textLabel?.text = memes[indexPath.row].topText
+        cell.imageView?.image = memes[indexPath.row].memedImage
+        
+        return cell
+    }
 }
